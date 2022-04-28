9ca3f35b3b1895bd3131a8b3d47a0515d9ab77c8
Axel Almonte <aalmontecu@unsa.edu.pe>
2022-04-28 11:33:46 -0500

    se agrego el nombre del auytor del codigo

--- hola.java
+++ hola.java
@@ -1,7 +1,7 @@
 
 class main {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("hola mundo" + "\n mi nombre es axel frank almonte cuba");
     }
 
 }
